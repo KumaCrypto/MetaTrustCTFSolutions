50911efd3208d4790a354b4a4d805a3188d55691
Vladimir Kumalagov <vkumalagov@icloud.com>
2023-10-01 10:50:40 +0300

    feat: add explanation to ETHStaking

--- test/ETHStaking.t.sol
+++ test/ETHStaking.t.sol
@@ -4,12 +4,36 @@ pragma solidity ^0.8.0;
 import {Test} from "forge-std/Test.sol";
 import {Challenge, StakingPool} from "../src/ETHStaking/Challenge.sol";
 
+// In this task, we need to empty the insurance contract. We can do this through the `requestCompensation` function. However, to do this, one needs to be on the `protectedContracts` list, which can be achieved by calling the `registerContract` function. This function includes a check to ensure that `msg.sender` has identical bytecode to the StakingPool contract. Therefore, we must analyze the StakingPool code to find possible vulnerabilities.
+
+// To withdraw funds from the `Insurance` contract, we need to invoke the `endOperatorService` function, which can only be called in the `Validating` state. To transition the contract to this state, one must call the `createValidator` function, and so on. You can follow the chain and come to the conclusion that it's impossible : )
+
+// What can we do about this?
+// To successfully call the `endOperatorService` function, we need the `Validating` state and pass the following check:
+// ```
+// require(
+//     (msg.sender == operator && block.timestamp > exitDate) ||
+//     (deposits[msg.sender] > 0 && block.timestamp > exitDate + MAX_SECONDS_IN_EXIT_QUEUE),
+//     "Permission denied or wrong time"
+// );
+// ```
+
+// We can see that the first part of the expression (before the `||` sign) cannot be satisfied by us, as we cannot be the operator; this limitation comes from the Insurance contract here.
+
+// This means our balance must be positive and 12 weeks must have passed.
+
+// Without delving into lengthy explanations, the final solution to pass this check is:
+// We can deploy our own StakingPool with a positive balance for our account and the Validation state. However, if we simply add changes to these variables in the constructor, the bytecode will not match the original StakingPool.
+
+// Here, we can resort to a trick and substitute the contract's bytecode in the constructor. This is possible because the final bytecode of the contract is implicitly returned at the end of the constructor's execution. We can achieve this using inline assembly.
+// Therefore, all we need to do is modify the variables in the constructor and return the StakingPool bytecode.
+
 contract ETHStakingTest is Test {
     Challenge private challenge;
     Solver private solver;
 
     function setUp() external {
-        skip(123123123); // Set block.timestamp (foundry has small default block.timestamp)
+        skip(123123123); // Set block.timestamp (foundry has small default block.timestamp, that is less than 12 weeks)
 
         challenge = new Challenge{value: 10 ether}();
         solver = new Solver(address(challenge), address(challenge.insurance()));
@@ -21,8 +45,6 @@ contract ETHStakingTest is Test {
 
         assertTrue(challenge.isSolved());
     }
-
-    receive() external payable {}
 }
 
 // Inherit from StakingPool to save the storage layout
